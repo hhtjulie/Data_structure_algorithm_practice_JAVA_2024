cc3695c5d5229c0af8c7eeb60592ea622e6cd783
Juli Hsu <juliehsu0226@gmail.com>
2024-04-04 16:14:43 +0800

    Update Deque.java

--- Deque.java
+++ Deque.java
@@ -4,40 +4,40 @@ public class Deque<E> {
     private int front, tail;
     private int size;
 
-    public Deque(int capacity){
-        data = (E[])new Object[capacity]; 
+    public Deque(int capacity) {
+        data = (E[]) new Object[capacity];
         front = 0;
         tail = 0;
         size = 0;
     }
 
-    public Deque(){
+    public Deque() {
         this(10);
     }
 
-    public int getCapacity(){
+    public int getCapacity() {
         return data.length;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
-    public void addLast(E e){
-        if(size == getCapacity()){
+    public void addLast(E e) {
+        if (size == getCapacity()) {
             resize(getCapacity() * 2);
         }
         data[tail] = e;
         tail = (tail + 1) % data.length;
-        size ++;
+        size++;
     }
 
-    public void addFront(E e){
-        if(size == getCapacity()){
+    public void addFront(E e) {
+        if (size == getCapacity()) {
             resize(getCapacity() * 2);
         }
         front = front == 0 ? data.length - 1 : front - 1;
@@ -45,49 +45,49 @@ public class Deque<E> {
         size++;
     }
 
-    public E removeFront(){
-        if(isEmpty()){
+    public E removeFront() {
+        if (isEmpty()) {
             throw new IllegalArgumentException("deque is empty. no element to remove.");
         }
         E e = data[front];
         data[front] = null; //
-        front = (front+1)%data.length;
-        size --;
-        if(size == getCapacity()/4 && getCapacity() / 2 != 0) //
+        front = (front + 1) % data.length;
+        size--;
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) //
             resize(getCapacity() / 2);
         return e;
     }
 
-    public E removeLast(){
-        if(isEmpty()){
+    public E removeLast() {
+        if (isEmpty()) {
             throw new IllegalArgumentException("deque is empty. no element to remove.");
         }
-        tail = tail == 0? data.length - 1 : tail -1; //
+        tail = tail == 0 ? data.length - 1 : tail - 1; //
         E e = data[tail];
         data[tail] = null;
-        size --;
-        if(size == getCapacity()/4 && getCapacity()/2!=0){
-            resize(getCapacity()/2);
+        size--;
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+            resize(getCapacity() / 2);
         }
         return e;
     }
 
-    public E getFront(){
-        if(isEmpty())
+    public E getFront() {
+        if (isEmpty())
             throw new IllegalArgumentException("deque is empty. no front element.");
         return data[front];
     }
 
-    public E getLast(){
-        if(isEmpty())
+    public E getLast() {
+        if (isEmpty())
             throw new IllegalArgumentException("deque is empty. no tail element.");
         int idx = tail == 0 ? data.length - 1 : tail - 1;
         return data[idx];
     }
 
-    private void resize(int newCapacity){
+    private void resize(int newCapacity) {
         E[] newData = (E[]) new Object[newCapacity + 1];
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             newData[i] = data[(front + i) % data.length];
         }
 
@@ -97,31 +97,34 @@ public class Deque<E> {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder str = new StringBuilder();
         str.append(String.format("Queue: size = %d , capacity = %d\n", size, getCapacity()));
         str.append("front [");
-        for(int i = front; i!=tail ; i = (i+1) % data.length){
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
             str.append(data[i]);
-            if((i + 1) % data.length != tail){
+            if ((i + 1) % data.length != tail) {
                 str.append(", ");
             }
         }
         str.append("] tail");
         return str.toString();
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Deque<Integer> dq = new Deque<>();
-        for(int i = 0 ; i < 16 ; i ++){
-            if(i % 2 == 0) dq.addLast(i);
-            else dq.addFront(i);
+        for (int i = 0; i < 16; i++) {
+            if (i % 2 == 0)
+                dq.addLast(i);
+            else
+                dq.addFront(i);
             System.out.println(dq);
         }
         System.out.println();
-        for(int i = 0; !dq.isEmpty(); i ++){
-            if(i % 2 == 0) 
+        for (int i = 0; !dq.isEmpty(); i++) {
+            if (i % 2 == 0)
                 dq.removeFront();
-            else 
+            else
                 dq.removeLast();
             System.out.println(dq);
         }
